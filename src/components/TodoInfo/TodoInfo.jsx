b22aab0d94d9ef9f5ab1b3242297cff6968f9ce6
Nadiia Chernygina <nadja.chernygina@gmail.com>
2025-08-26 15:10:35 +0300

    Added a visible indicator of completion status near the title

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -6,6 +6,8 @@ export const TodoInfo = ({ todo }) => (
     className={`TodoInfo ${todo.completed === true ? 'TodoInfo--completed' : ''}`}
   >
     <h2 className="TodoInfo__title">{todo.title}</h2>
+    <span className="TodoInfo__status">{todo.completed ? 'Completed' : 'Not completed'}</span>
+    { ' ' }
     {todo.user && <UserInfo user={todo.user} />}
   </article>
 );
